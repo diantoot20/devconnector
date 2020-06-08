0a12923cd8cda4bc0b4713a45f9043aba0e8db4a
Adrian Jr <diantoot@gmail.com>
2020-06-08 11:30:54 +0400

    Fixed the '.split is not a function' error

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -70,8 +70,16 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+
+    try {
+      if (skills) {
+        profileFields.skills = skills
+          .toString()
+          .split(",")
+          .map((skill) => skill.trim());
+      }
+    } catch (err) {
+      console.log(err.message);
     }
 
     //Build social Array
